064ff058ef5b7000d791bb22d97c892eba777172
Kirim-lang <jorgefeliperodrigues841@gmail.com>
2025-08-22 17:45:06 -0300

    Commit inicial - Projeto Gestor de Despesas

---
+++ Projeto Gestor/demo/src/main/java/com/GestordeDespesasPessoais/demo/GestorDeDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.GestordeDespesasPessoais.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeDespesasApplication.class, args);
+	}
+
+}
---
+++ Projeto Gestor/demo/src/test/java/com/GestordeDespesasPessoais/demo/GestorDeDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestordeDespesasPessoais.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
